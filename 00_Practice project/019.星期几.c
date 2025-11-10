e15f61cb9b1148a056cd009f22f16b1f3ed0a27d
heyeepc <2675458141@qq.com>
2025-11-11 01:03:05 +0800

    Create 019.星期几.c

---
+++ 00_Practice project/019.星期几.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    printf("输入一个整数");
+    scanf("%d", &a);
+
+    switch (a) {
+        case 1:
+            printf("星期一");
+            break;
+        case 2:
+            printf("星期二");
+            break;
+        case 3:
+            printf("星期三");
+            break;
+        case 4:
+            printf("星期四");
+            break;
+        case 5:
+            printf("星期五");
+            break;
+        case 6:
+            printf("星期六");
+            break;
+        case 7:
+            printf("星期日");
+            break;
+        default:
+            printf("不属于任何星期");
+            break;
+    }
+    return 0;
+}
