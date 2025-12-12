482e6254952ac1f80f007258b03b141231d67c73
heyeepc <2675458141@qq.com>
2025-12-12 12:48:34 +0800

    Create 005_隔着一个字符串复制.py

---
+++ 01_Review questions/005_隔着一个字符串复制.py
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void my_strcpy(char *dest, char *src) {
+
+    while ( *src != '\0') {
+
+
+        *dest = *src;
+        src += 2;
+        dest++;
+
+    }
+
+    *dest  = '\0';
+}
+
+int main(){
+
+    char a[20];
+    my_strcpy(a, "world");
+    printf("%s\n", a);   // world
+
+    return 0;
+
+}
