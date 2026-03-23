48c8613b1c084cdda31661c76fce837555356240
heyeepc <2675458141@qq.com>
2026-03-23 14:03:07 +0800

    Create 006_数组26字母.c

---
+++ 01_Review questions/006_数组26字母.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    
+    char alphabet[27] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','\0'};
+    
+
+    printf("%s\n", alphabet);
+    
+    return 0;
+}
